232b80786dfffb52215dd8ec19470cd215a5b84b
Sujitaghlan <sujitaghlan7@gmail.com>
2025-11-22 21:21:57 +0545

    Updated cart

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -54,4 +54,29 @@ const {Cart, Product} = require("../models");
   }
 };
 
-module.exports = {addToCart, getCart, removeFromCart};
+const decreaseQuantity = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const userId = req.user._id;
+
+    const cart = await Cart.findOne({ user: userId });
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+
+    const item = cart.items.find((i) => i.product.toString() === productId);
+    if (!item) return res.status(404).json({ message: "Item not found in cart" });
+
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+    } else {
+      cart.items = cart.items.filter((i) => i.product.toString() !== productId);
+    }
+
+    await cart.save();
+    res.status(200).json({ message: "Quantity updated", cart });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+module.exports = {addToCart, getCart, removeFromCart, decreaseQuantity};
