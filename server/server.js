e6c742a88e6b20f9914be647c1e3087ea44ce3f8
jivan-mukhiya <your-email@example.com>
2025-11-24 14:46:57 +0545

    update

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { connectDb } = require("./connection/connection");
-
-const app = express();
-app.use(express.json());
-
-connectDb();
-
-app.listen(3000, () => console.log("Server running on port 3000"));
