d3c8b769a52265b936b1e7ef4827d9b5823bfdb4
Sujitaghlan <sujitaghlan7@gmail.com>
2025-12-21 16:12:46 +0545

    fix service acc

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,11 +1,5 @@
 const admin = require("firebase-admin");
-let serviceAccount;
-
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  serviceAccount = require("../config/firebaseServiceAccountKey.json");
-}
+let serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
